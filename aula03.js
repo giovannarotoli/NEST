e0874269f280dd3539a1a1fbe67de49ba0cdc8ae
giovannarotoli <giovanarotolireis@gmail.com>
2023-06-14 10:28:30 -0300

    Alterado

---
+++ aula03.js
@@ -0,0 +1,42 @@
+const lista = [
+    {
+        nome:'Jose',
+        idade: 13
+    },
+    {
+        nome:'Luana',
+        idade: 13
+    },
+    {
+        nome:'Adriano',
+        idade: 26
+    },
+    {
+        nome:'Jose Arnaldo',
+        idade: 23
+    },
+    {
+        nome:'Anderson',
+        idade: 14
+    }, 
+    {
+        nome:'Ana',
+        idade: 16
+    }, 
+    {
+        nome:'Gabriel',
+        idade: 18
+    },
+    {
+        nome:'Adriana',
+        idade: 12
+    },
+]
+
+const  filtraPessoa = (pessoa) => pessoa.nome === 'Adriano';
+
+const listaFiltrada = lista.filter(filtraPessoa);
+
+console.log(lista);
+
+console.log(listaFiltrada)
