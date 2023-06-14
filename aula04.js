44a51ca74bf965a7af8e56c7aefe7d61df82dd44
giovannarotoli <giovanarotolireis@gmail.com>
2023-06-14 11:03:35 -0300

    Array find

---
+++ aula04.js
@@ -0,0 +1,38 @@
+const lista = [
+    {
+        nome:'Jose',
+        idade: 13
+    },
+    {
+        nome:'Luana',
+        idade: 13
+    },
+    {
+        nome:'Adriano',
+        idade: 26
+    },
+    {
+        nome:'Jose Arnaldo',
+        idade: 23
+    },
+    {
+        nome:'Anderson',
+        idade: 14
+    }, 
+    {
+        nome:'Ana',
+        idade: 16
+    }, 
+    {
+        nome:'Gabriel',
+        idade: 18
+    },
+    {
+        nome:'Adriana',
+        idade: 12
+    },
+]
+
+const novaPessoa = lista.find((pessoa) => pessoa.idade > 30);
+
+console.log(novaPessoa);
